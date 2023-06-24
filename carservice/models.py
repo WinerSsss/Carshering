a6c5249c8ad1922879f731ce7715a47742aa1434
karpiotr90 <92075179+karpiotr90@users.noreply.github.com>
2023-06-24 09:24:12 +0200

    delete timezone

--- carservice/models.py
+++ carservice/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.contrib.auth.models import User
-from django.utils import timezone
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
