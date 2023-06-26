78c8f14b15d9ad370b042b9453a288fcf8a4667e
karpiotr90 <92075179+karpiotr90@users.noreply.github.com>
2023-06-26 22:49:22 +0200

    fixed the fucked up main and add files from my wrong merged branch

--- carservice/views.py
+++ carservice/views.py
@@ -2,27 +2,46 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic, View
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-
 from .models import Car, Offer, Rent
-
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.timezone import now
+from datetime import timedelta
 from django.utils import timezone
-
 from . forms import CarUpdateForm, CarDeleteForm, OfferUpdateForm, OfferDeleteForm
 
 
-class CarCreateView(CreateView):
+class CarCreateView(LoginRequiredMixin, CreateView):
     model = Car
-    fields = ['serial_number', 'car_mileage', 'car_model', 'car_year']
+    fields = ['serial_number', 'car_mileage', 'car_brand', 'car_model', 'date_of_prod']
     template_name = 'car_create.html'
     success_url = reverse_lazy('car_read')
 
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['date_of_prod'] = 'YYYY-MM-DD'
+        return initial
 
-class CarReadView(View):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class CarReadView(LoginRequiredMixin, View):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Car.objects.all()
+        else:
+            return Car.objects.filter(user=self.request.user)
+
     def get(self, request):
+        cars = self.get_queryset()
         return render(
             request, template_name='car_read.html',
-            context={'cars': Car.objects.all()}
+            context={'cars': cars}
         )
 
 class CarUpdateView(View):
@@ -56,15 +75,31 @@ class CarDeleteView(View):
         return render(request, 'car_delete.html', {'form': form})
 
 
-class OfferCreateView(CreateView):
+class OfferCreateView(LoginRequiredMixin, CreateView):
     model = Offer
     fields = ['car', 'description', 'price']
     template_name = 'offer_create.html'
     success_url = reverse_lazy('offer_read')
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
-class OfferReadView(View):
+
+class OfferReadView(LoginRequiredMixin, View):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Offer.objects.all()
+        else:
+            return Offer.objects.filter(user=self.request.user)
+
     def get(self, request):
+        offers = self.get_queryset()
+
         return render(
             request, template_name='offer_read.html',
             context={'offers': Offer.objects.all()}
@@ -101,16 +136,35 @@ class OfferDeleteView(View):
         return render(request, 'offer_delete.html', {'form': form})
 
 
-class RentCreateView(CreateView):
+class RentCreateView(LoginRequiredMixin, CreateView):
     model = Rent
-    fields = ['status', 'rent_start', 'rent_stop', 'offer', 'user']
+    fields = ['rent_start', 'duration', 'offer']
     template_name = 'rent_create.html'
     success_url = reverse_lazy('rent_read')
 
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['rent_start'] = now()
+        return initial
 
-class RentListView(View):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class RentListView(LoginRequiredMixin, View):
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Rent.objects.all()
+        else:
+            return Rent.objects.filter(user=self.request.user)
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
     def get(self, request):
-        rents = Rent.objects.all()
+        rents = self.get_queryset()
 
         return render(
             request,
