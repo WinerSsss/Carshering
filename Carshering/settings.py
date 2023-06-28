61e623b979c95f86450220ea8b36acb06c78b142
jkfku <72601392+jkfku@users.noreply.github.com>
2023-06-28 19:29:58 +0200

    db2-db-change

--- Carshering/settings.py
+++ Carshering/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = "Carshering.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db2.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
