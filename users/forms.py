963d025b00ef83da32b5b16bae1536c694a1efbb
Robert Khurshudyan <roberthurshudyan@gmail.com>
2023-07-01 03:20:26 +0200

    Add some changes in profile and name of carshering and the picture

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
 from django import forms
 from django.utils.translation import gettext_lazy as _
-
 User = get_user_model()
 
 
@@ -10,7 +9,8 @@ class UserCreationForm(UserCreationForm):
     email = forms.EmailField(
         label=_("Email"),
         max_length=254,
-        widget=forms.EmailInput(attrs={'autocomplete': 'email'})
+        widget=forms.EmailInput(attrs={'autocomplete': 'email'}),
+        required=True
     )
 
     class Meta(UserCreationForm.Meta):
@@ -19,6 +19,7 @@ class UserCreationForm(UserCreationForm):
 
 
 class UserUpdateForm(forms.ModelForm):
+
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
@@ -29,3 +30,4 @@ class CustomPasswordResetForm(PasswordResetForm):
         label="Email",
         widget=forms.EmailInput(attrs={'class': 'form-control'}),
     )
+
