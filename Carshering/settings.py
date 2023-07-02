80d027c585cf82a38f096b899435a849bf3bf360
Quaintart <115464925+Quaintart@users.noreply.github.com>
2023-07-02 09:35:35 +0200

    Updated all templates and added Crispy

--- Carshering/settings.py
+++ Carshering/settings.py
@@ -46,7 +46,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'carservice',
     "users",
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
